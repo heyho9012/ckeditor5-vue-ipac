0df415794b7c7571b5809852cf82dfd659571e99
john9012 <john9012@ipactory.com>
2023-03-11 16:21:39 +0900

    Fix postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -7,12 +7,14 @@
 
 /* eslint-env node */
 
-const path = require( 'path' );
-const fs = require( 'fs' );
+import path from 'path';
+import fs from 'fs';
+import husky from 'husky';
+
 const ROOT_DIRECTORY = path.join( __dirname, '..' );
 
 // When installing a repository as a dependency, the `.git` directory does not exist.
 // In such a case, husky should not attach its hooks as npm treats it as a package, not a git repository.
 if ( fs.existsSync( path.join( ROOT_DIRECTORY, '.git' ) ) ) {
-	require( 'husky' ).install();
+	husky.install();
 }
