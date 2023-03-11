9e20a6b039a894de79d304e4f212076e57483dad
john9012 <john9012@ipactory.com>
2023-03-11 16:03:48 +0900

    fix ckeditor.js

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -6,10 +6,10 @@
 /* global window, console */
 
 import { h, markRaw } from 'vue';
-import { debounce } from 'lodash-es';
+// import { debounce } from 'lodash-es';
 
 const SAMPLE_READ_ONLY_LOCK_ID = 'Integration Sample';
-const INPUT_EVENT_DEBOUNCE_WAIT = 300;
+// const INPUT_EVENT_DEBOUNCE_WAIT = 300;
 
 export default {
 	name: 'ckeditor',
@@ -35,20 +35,20 @@ export default {
 		return h( this.tagName );
 	},
 
-	model: {
-		prop: 'modelValue',
-		event: 'update:modelValue'
-	},
+	// model: {
+	// 	prop: 'modelValue',
+	// 	event: 'update:modelValue'
+	// },
 
 	props: {
 		editor: {
 			type: Function,
 			default: null
 		},
-		modelValue: {
-			type: String,
-			default: ''
-		},
+		// modelValue: {
+		// 	type: String,
+		// 	default: ''
+		// },
 		config: {
 			type: Object,
 			default: () => ( {} )
@@ -81,9 +81,9 @@ export default {
 		// https://github.com/ckeditor/ckeditor5-vue/issues/101
 		const editorConfig = Object.assign( {}, this.config );
 
-		if ( this.modelValue ) {
-			editorConfig.initialData = this.modelValue;
-		}
+		// if ( this.modelValue ) {
+		// 	editorConfig.initialData = this.modelValue;
+		// }
 
 		this.editor.create( this.$el, editorConfig )
 			.then( editor => {
@@ -94,9 +94,9 @@ export default {
 
 				// Synchronize the editor content. The #modelValue may change while the editor is being created, so the editor content has
 				// to be synchronized with these potential changes as soon as it is ready.
-				if ( this.modelValue !== editorConfig.initialData ) {
-					editor.setData( this.modelValue );
-				}
+				// if ( this.modelValue !== editorConfig.initialData ) {
+				// 	editor.setData( this.modelValue );
+				// }
 
 				// Set initial disabled state.
 				if ( this.disabled ) {
@@ -123,33 +123,33 @@ export default {
 	},
 
 	watch: {
-		modelValue( value ) {
-			// Synchronize changes of #modelValue. There are two sources of changes:
-			//
-			//                External modelValue change      ──────╮
-			//                                                      ╰─────> ┏━━━━━━━━━━━┓
-			//                                                              ┃ Component ┃
-			//                                                      ╭─────> ┗━━━━━━━━━━━┛
-			//                   Internal data change         ──────╯
-			//             (typing, commands, collaboration)
-			//
-			// Case 1: If the change was external (via props), the editor data must be synced with
-			// the component using instance#setData() and it is OK to destroy the selection.
-			//
-			// Case 2: If the change is the result of internal data change, the #modelValue is the
-			// same as this.lastEditorData, which has been cached on #change:data. If we called
-			// instance#setData() at this point, that would demolish the selection.
-			//
-			// To limit the number of instance#setData() which is time-consuming when there is a
-			// lot of data we make sure:
-			//    * the new modelValue is at least different than the old modelValue (Case 1.)
-			//    * the new modelValue is different than the last internal instance state (Case 2.)
-			//
-			// See: https://github.com/ckeditor/ckeditor5-vue/issues/42.
-			if ( this.instance && value !== this.lastEditorData ) {
-				this.instance.setData( value );
-			}
-		},
+		// modelValue( value ) {
+		// Synchronize changes of #modelValue. There are two sources of changes:
+		//
+		//                External modelValue change      ──────╮
+		//                                                      ╰─────> ┏━━━━━━━━━━━┓
+		//                                                              ┃ Component ┃
+		//                                                      ╭─────> ┗━━━━━━━━━━━┛
+		//                   Internal data change         ──────╯
+		//             (typing, commands, collaboration)
+		//
+		// Case 1: If the change was external (via props), the editor data must be synced with
+		// the component using instance#setData() and it is OK to destroy the selection.
+		//
+		// Case 2: If the change is the result of internal data change, the #modelValue is the
+		// same as this.lastEditorData, which has been cached on #change:data. If we called
+		// instance#setData() at this point, that would demolish the selection.
+		//
+		// To limit the number of instance#setData() which is time-consuming when there is a
+		// lot of data we make sure:
+		//    * the new modelValue is at least different than the old modelValue (Case 1.)
+		//    * the new modelValue is different than the last internal instance state (Case 2.)
+		//
+		// See: https://github.com/ckeditor/ckeditor5-vue/issues/42.
+		// 	if ( this.instance && value !== this.lastEditorData ) {
+		// 		this.instance.setData( value );
+		// 	}
+		// },
 
 		// Synchronize changes of #disabled.
 		disabled( readOnlyMode ) {
@@ -169,22 +169,22 @@ export default {
 			// Failing to do so leads to race conditions, for instance, when the component modelValue
 			// is set twice in a time span shorter than the debounce time.
 			// See https://github.com/ckeditor/ckeditor5-vue/issues/149.
-			const emitDebouncedInputEvent = debounce( evt => {
-				// Cache the last editor data. This kind of data is a result of typing,
-				// editor command execution, collaborative changes to the document, etc.
-				// This data is compared when the component modelValue changes in a 2-way binding.
-				const data = this.lastEditorData = editor.getData();
+			// const emitDebouncedInputEvent = debounce( evt => {
+			// Cache the last editor data. This kind of data is a result of typing,
+			// editor command execution, collaborative changes to the document, etc.
+			// This data is compared when the component modelValue changes in a 2-way binding.
+			// const data = this.lastEditorData = editor.getData();
 
-				// The compatibility with the v-model and general Vue.js concept of input–like components.
-				this.$emit( 'update:modelValue', data, evt, editor );
-				this.$emit( 'input', data, evt, editor );
-			}, INPUT_EVENT_DEBOUNCE_WAIT, { leading: true } );
+			// The compatibility with the v-model and general Vue.js concept of input–like components.
+			// 	this.$emit( 'update:modelValue', data, evt, editor );
+			// 	this.$emit( 'input', data, evt, editor );
+			// }, INPUT_EVENT_DEBOUNCE_WAIT, { leading: true } );
 
 			// Debounce emitting the #input event. When data is huge, instance#getData()
 			// takes a lot of time to execute on every single key press and ruins the UX.
 			//
 			// See: https://github.com/ckeditor/ckeditor5-vue/issues/42
-			editor.model.document.on( 'change:data', emitDebouncedInputEvent );
+			// editor.model.document.on( 'change:data', emitDebouncedInputEvent );
 
 			editor.editing.view.document.on( 'focus', evt => {
 				this.$emit( 'focus', evt, editor );
